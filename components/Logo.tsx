081183bb9ea5391fa80d4372bf3f231ccf4155ee
Diwan Malla <diwanmalla@192-168-1-2.tpgi.com.au>
2024-11-10 15:12:10 +1100

    Added mobile navbar

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -13,5 +13,15 @@ const Logo = () => {
     </div>
   );
 };
-
+export const LogoMobile = () => {
+  return (
+    <div>
+      <Link href={"/"} className="flex items-center gap-2">
+        <p className="bg-gradient-to-r from-amber-400 to-orange-500 bg-clip-text text-3xl font-bold leading-tight tracking-tighter text-transparent">
+          SMART SPEND
+        </p>
+      </Link>
+    </div>
+  );
+};
 export default Logo;
